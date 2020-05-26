3128737e1fa9fc41419609825e0e7ada915830dc
tianhe1986 <w1s2j3229@163.com>
2020-05-26 11:26:01 +0800

    微信小程序 + laya的demo，包括展示图片，文字，播放spine动画，画图形，点击触发事件。
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- demo-laya/index/index.js
+++ demo-laya/index/index.js
@@ -1,26 +1,40 @@
-import { getSpine } from '../loaders/spine-webgl.js'
-
-const { renderAnimation } = require('../test-cases/animation')
 const { layaTest } = require('../test-cases/layatest')
 
 const app = getApp()
 
 Page({
-  data: {},
+  data: {
+    isHidden: false
+  },
   onLoad: function () {
     wx.createSelectorQuery()
-      .select('#webgl')
-      .node().exec((res) => {
-        const canvas = res[0].node;
-        const dpr = wx.getSystemInfoSync().pixelRatio
-        var width = canvas.width;
-        var height = canvas.height;
-        canvas.width = width * dpr
-        canvas.height = height * dpr
-        const spine = getSpine(canvas);
-        // renderAnimation(canvas, spine)
-        layaTest(canvas);
-      })
+      .select('#test2d')
+      .node().exec((res2d) => {
+        const canvas2d = res2d[0].node;
+        wx.createSelectorQuery()
+        .select('#testchar')
+        .node().exec((reschar) => {
+          const canvasChar = reschar[0].node;
+          wx.createSelectorQuery()
+            .select('#webgl')
+            .node().exec((res) => {
+              this.setData({
+                isHidden: true
+              });
+              const canvas = res[0].node;
+              const dpr = wx.getSystemInfoSync().pixelRatio
+              console.log(dpr, canvas.width);
+              var width = canvas.width;
+              var height = canvas.height;
+              canvas.width = width * dpr
+              canvas.height = height * dpr
+              layaTest(canvas, canvas2d, canvasChar);
+            });
+        });
+        
+      });
+
+    
   },
 
   doTouchStart(event) {
--- demo-laya/test-cases/layatest.js
+++ demo-laya/test-cases/layatest.js
@@ -1,4 +1,4 @@
-export function layaTest(canvas) {
+export function layaTest(canvas, canvas2d, canvasChar) {
   var Laya = wx.window.Laya;
   class Test {
       constructor() {
@@ -8,12 +8,12 @@ export function layaTest(canvas) {
 
       init() {
         wx._cacheCanvas = canvas;
-        
-        Laya.init(canvas, 750, 1334, Laya["WebGL"]);
-
+        Laya.Config.isAlpha = true;
+        // 传递的三个参数分别用于主画布，Browser额外画布，以及处理文字用画布
+        Laya.init(canvas, canvas2d, canvasChar, 750, 1334, Laya["WebGL"]);
+        Laya.stage.bgColor = null;
         Laya.stage.screenMode = "vertical";
-        Laya.stage.scaleMode = "noscale";
-        Laya.stage.screenMode = "none";
+        Laya.stage.scaleMode = "showall";
         Laya.stage.alignV = "middle";
         Laya.stage.alignH = "center";
         //兼容微信不支持加载scene后缀场景
@@ -24,25 +24,49 @@ export function layaTest(canvas) {
           var sp = new Laya.Sprite();
           Laya.stage.addChild(sp);
           //画直线
-          sp.graphics.drawLine(10, 58, 375, 58, "#ffff00", 5);
+          sp.graphics.drawLine(10, 58, 500, 58, "#ffff00", 5);
 
-          sp.width = 375;
+          sp.width = 500;
           sp.height = 200;
           sp.on(Laya.Event.CLICK, this, this.showAni);
+
+          var image = new Laya.Image("/res/0.png");
+          Laya.stage.addChild(image);
+          image.pos(100, 300);
+          image.width = 300;
+          image.height = 300;
+          image.on(Laya.Event.CLICK, this, this.showAni);
+
+          var text = new Laya.Text();
+          text.fontSize = 48;
+          text.text = "你这小家伙";
+          text.color = "red";
+          Laya.stage.addChild(text);
+          text.pos(200, 700);
+          text.on(Laya.Event.CLICK, this, this.showAni);
+
+          var text = new Laya.Text();
+          text.fontSize = 48;
+          text.text = "你这大家伙";
+          text.color = "blue";
+          Laya.stage.addChild(text);
+          text.pos(200, 1000);
+          text.on(Laya.Event.CLICK, this, this.showAni);
       }
 
       showAni() {
         var skeleton = new Laya.Skeleton();
         //添加到舞台
         Laya.stage.addChild(skeleton);
-        skeleton.pos(400,800);
+        skeleton.pos(300,1000);
         skeleton.scale(0.7, 0.7);
-        //通过加载直接创建动画
-        skeleton.load("http://zmtest.rzcdz2.com/xiaoshuo/map-test/spine/laya/aar_hongbaotiaochu.sk", Laya.Handler.create(this, () => {
+        // 通过加载直接创建动画
+        // 注意必须是远程加载才行，因此需要自己搭建web服务器
+        skeleton.load("http://test.mine.cn/spine/spineboy.sk", Laya.Handler.create(this, () => {
           skeleton.player.on(Laya.Event.STOPPED, this, () => {
             skeleton.removeSelf();
           });
-          skeleton.play("play", false);
+          skeleton.play("jump", false);
         }));
       }
   }
